423515dd4ca546e07dcdd090ac95a49a644b0daa
AartiW-12 <aartiwhaval12@gmail.com>
2025-10-31 09:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const p = document.getElementById("timer");
+
+function updateTimer(){
+	const date = new Date();
+
+	let time = date.toLocaleString();
+	
+	p.textContent = time;
+};
+setInterval(()=>{
+	updateTimer();
+},1000)
